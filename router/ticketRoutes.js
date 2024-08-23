9dc0e0931be718525ac2a03c65e3a224ecaa7ee6
shurik302 <balabuha1988@gmail.com>
2024-08-23 15:44:43 +0200

    fix:

--- router/ticketRoutes.js
+++ router/ticketRoutes.js
@@ -8,7 +8,7 @@ const jwt = require('jsonwebtoken');
 const UserModel = require('../models/user-model');
 const bot = require("../bot");
 
-const adminChatIds = ['5581268424', '1067260096', '7384793886'];
+const adminChatIds = ['5581268424', '1067260096', '7384793886', '413437332'];
 
 // Middleware для перевірки токена
 const verifyToken = async (req, res, next) => {
